8b637c057690e0f644a9cb42f214ceea41b16ff9
kerem37mert <kerem37mert@yahoo.com>
2026-02-13 16:31:11 +0300

    [ADD] Methodology Screen created

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Methodology from "./pages/Methodology";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Header title="Havelsan Suit Project Demo" />
-            <Routes>
-                <Route index element={ <Home /> } />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header title="Havelsan Suit Project Demo" />
+      <Routes>
+        <Route index element={ <Home /> } />
+        <Route path="/methodology" element={ <Methodology /> } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,15 +3,15 @@ import type { HeaderProps } from "./Header.types";
 import classes from "./Header.module.scss";
 
 const Header = ({ title }: HeaderProps) => {
-    return (
-        <div className={ classes.header }>
-            <div className={ classes.title }>{ title }</div>
-            <div className={ classes["menu-container"] }>
-                <Link className={ classes["menu-link"] } to={"/"} >Ana Sayfa</Link>
-                <Link className={ classes["menu-link"]} to={"/"} >Yöntem</Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className={ classes.header }>
+      <div className={ classes.title }>{ title }</div>
+      <div className={ classes["menu-container"] }>
+        <Link className={ classes["menu-link"] } to={"/"} >Ana Sayfa</Link>
+        <Link className={ classes["menu-link"]} to={"/methodology"} >Yöntem</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
